be08e70b5e7b779797e0a7633f7b2f1a7325f837
kimmoonjong <hushkmj@naver.com>
2024-03-16 15:40:40 +0900

    ## Gestures, Drag

--- animation/README.md
+++ animation/README.md
@@ -2,6 +2,7 @@
 
 ## Installation
 
+환경설정  
 https://www.framer.com/motion/
 
 npm i styled-components  
@@ -60,8 +61,8 @@ const Box = styled(motion.div)`
 
 ## Variants
 
-framer 속성들을 javascript Object 형식으로 사용할 수 있게 해주는 속성
-자식요소에게도 animation을 전달함
+프레이머 속성들을 자바스크립트 오브젝트 형식으로 사용할 수 있게 해주는 속성  
+자식요소에게도 애니메이션을 전달함
 
 ```
 const boxVariants = {
@@ -99,4 +100,64 @@ const circleVariants = {
     <Circle variants={circleVariants} />
     <Circle variants={circleVariants} />
 </Box>
+```
+
+## Gestures, Drag
+
+마우스 이벤트 애니메이션 부여
+
+```
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* overflow: hidden; */
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { borderRadius: "100px" },
+  drag: {
+    backgroundColor: "rgba(50, 255, 126,1.0)",
+    transition: { duration: 5 }, // 컴포넌트를 드래그중에 백그라운드컬러가 전환되는 시간을 설정한다.
+  },
+};
+
+<Wrapper>
+  {/* 아래의 BiggerBox는 biggerBoxRef 코드조각하고 연결됨 */}
+  <BiggerBox ref={biggerBoxRef}>
+    <Box
+      drag //컴포넌트를 드래그할 수 있게 하는 속성
+      dragSnapToOrigin // 드래그를 마친 컴포넌트가 본래 자리로 돌아감.
+      dragElastic={0.5} // 드래그중인 컴포넌트를 본래 자리로 '당기는 힘'
+      dragConstraints={biggerBoxRef} //드래그의 이동범위를 제약한다. (biggerBoxRef == BiggerBox)
+      variants={boxVariants}
+      whileHover={"hover"} // 컴포넌트에 마우스를 올려둘때의 이벤트 속성
+      whileTap={"click"} // 컴포넌트에 마우스를 클릭했을때의 이벤트 속성
+      whileDrag={"drag"}
+    />
+  </BiggerBox>
+</Wrapper>
 ```
--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -9,63 +10,53 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* overflow: hidden; */
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5, // 자식컴포넌트들에게 딜레이를 줌
-      staggerChildren: 0.5, // 자식컴포넌트들에게 순번을 줌
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { borderRadius: "100px" },
+  drag: {
+    backgroundColor: "rgba(50, 255, 126,1.0)",
+    transition: { duration: 5 }, // 컴포넌트를 드래그중에 백그라운드컬러가 전환되는 시간을 설정한다.
   },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null); // 특정 Element를 잡는 리액트 훅
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      {/* 아래의 BiggerBox는 biggerBoxRef 코드조각하고 연결됨 */}
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag //컴포넌트를 드래그할 수 있게 하는 속성
+          dragSnapToOrigin // 드래그를 마친 컴포넌트가 본래 자리로 돌아감.
+          dragElastic={0.5} // 드래그중인 컴포넌트를 본래 자리로 '당기는 힘'
+          dragConstraints={biggerBoxRef} //드래그의 이동범위를 제약한다. (biggerBoxRef == BiggerBox)
+          variants={boxVariants}
+          whileHover={"hover"} // 컴포넌트에 마우스를 올려둘때의 이벤트 속성
+          whileTap={"click"} // 컴포넌트에 마우스를 클릭했을때의 이벤트 속성
+          whileDrag={"drag"}
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
